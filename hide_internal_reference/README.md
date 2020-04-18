52350bfe586c03f6c534b40bb8ac60ecac1a7d6f
Tri Nanda <zidanecr7kaka@gmail.com>
2020-04-18 08:44:11 +0700

    specify the branch before upload to apps.odoo.com

---
+++ hide_internal_reference/README.md
@@ -0,0 +1,3 @@
+# Odoo13-Hide-Internal-Reference
+Currently in Odoo the product internal reference appear beside the product name beside sale order, invoice, etc.. 
+Like this "[CODE] Product Name". Using this module we hide the problematic.
---
+++ hide_internal_reference/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hide_internal_reference",
+
+    'summary': """
+        Hide Product Internal Reference
+        """,
+
+    'description': """
+        Currently in Odoo the product internal reference appear beside the product name beside sale order, invoice, etc.. 
+        Like this "[CODE] Product Name". Using this module we hide the problematic.
+    """,
+
+    'author': "Lima Bersaudara",
+    'website': "https://github.com/trinanda",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
