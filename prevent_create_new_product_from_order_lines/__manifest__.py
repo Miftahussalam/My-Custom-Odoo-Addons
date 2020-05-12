4c83fcf4dde204d230513c4357736940bf44eca7
Tri Nanda <zidanecr7kaka@gmail.com>
2020-05-12 08:20:40 +0700

    prevent create new product from purchase lines

--- prevent_create_new_product_from_order_lines/__manifest__.py
+++ prevent_create_new_product_from_order_lines/__manifest__.py
@@ -19,16 +19,17 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Utilities',
-    'version': '0.1',
+    'version': '13.0.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale'],
+    'depends': ['sale', 'purchase'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/sale_views.xml',
+        'views/purchase_views.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- prevent_create_new_product_from_order_lines/models/models.py
+++ prevent_create_new_product_from_order_lines/models/models.py
@@ -3,3 +3,7 @@ from odoo import api, fields, models, osv
 
 class PreventCreateNewProductOnSalesOrderLines(osv.osv.Model):
     _inherit = 'sale.order'
+
+
+class PreventCreateNewProductOnPurchaseOrderLines(osv.osv.Model):
+    _inherit = 'purchase.order'
