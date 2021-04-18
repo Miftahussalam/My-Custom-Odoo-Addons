1adcd9db27a79436780d2d4813ee8ce7bdb3cfd5
Tri Nanda <zidanecr7kaka@gmail.com>
2021-04-18 15:25:46 +0700

    delete custom module v13

--- prevent_create_new_contact_from_order_form/models/models.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models, osv
-
-
-class PreventCreateNewContactFromSalesOrderForm(osv.osv.Model):
-    _inherit = 'sale.order'
-
-
-class PreventCreateNewContactFromPurchaseOrderForm(osv.osv.Model):
-    _inherit = 'purchase.order'
