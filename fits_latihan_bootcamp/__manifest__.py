f6ae274147bfd4f3cc065f8f15f1197b2abc20d5
Tri Nanda <zidanecr7kaka@gmail.com>
2021-04-18 15:27:22 +0700

    add author and website

--- fits_latihan_bootcamp/__manifest__.py
+++ fits_latihan_bootcamp/__manifest__.py
@@ -9,13 +9,13 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "da Ti Soft Consulting",
+    'website': "https://github.com/trinanda",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'HR',
+    'category': 'HR, Expense',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
