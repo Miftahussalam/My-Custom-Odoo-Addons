61dc7e5b8822b42f698e86624d727b88d3ca52b6
Tri Nanda <zidanecr7kaka@gmail.com>
2021-10-24 09:35:14 +0700

    pos_sales_details_advanced_report

---
+++ pos_sales_details_advanced_report/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "pos_sales_details_advanced_report",
+
+    'summary': """
+        Add cost price, sales prices and profit to POS Sales Details report
+        """,
+
+    'description': """
+        Add cost price, sales prices and profit to POS Sales Details report
+    """,
+
+    'author': "da Ti Soft Consulting",
+    'website': "https://github.com/trinanda",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Point of Sale',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['point_of_sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/report_salesdetails.xml',
+    ],
+}
