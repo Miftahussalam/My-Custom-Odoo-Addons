1a7919377d324bbc1d56944272a7632cb3d06a78
Tri Nanda <zidanecr7kaka@gmail.com>
2021-10-24 10:03:06 +0700

    add pos_sales_details_advanced_report icon and LICENSE

--- pos_sales_details_advanced_report/__manifest__.py
+++ pos_sales_details_advanced_report/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "pos_sales_details_advanced_report",
+    'name': "POS Sales Details Advance Report",
 
     'summary': """
         Add cost price, sales prices and profit to POS Sales Details report
@@ -12,6 +12,7 @@
 
     'author': "da Ti Soft Consulting",
     'website': "https://github.com/trinanda",
+    'images': ['static/description/icon.png'],
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
