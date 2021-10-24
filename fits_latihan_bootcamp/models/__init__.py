c557bb181de2c735e4f34664899c2309a1fc7d9a
Tri Nanda <zidanecr7kaka@gmail.com>
2021-10-24 09:31:42 +0700

    delete fits_latihan_bootcam from branch 14.0

--- fits_latihan_bootcamp/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import employee
-from . import pengajuan_sppd
-from . import lphd
-from . import hr_expense
-
--- fits_latihan_bootcamp/models/hr_expense.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-
-class HrExpenseSheet(models.Model):
-    _inherit = 'hr.expense.sheet'
-    divisi_id = fields.Many2one(string='Divisi', related='employee_id.divisi_id', store=True)
