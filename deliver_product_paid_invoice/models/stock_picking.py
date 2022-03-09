9ae9a48cb0d2dabdf2248e1dc8d131f1b2901fab
Tri Nanda <zidanecr7kaka@gmail.com>
2022-03-09 11:04:41 +0700

    just delivery product when invoice paid if it have sale_order_id on stock.picking

--- deliver_product_paid_invoice/models/stock_picking.py
+++ deliver_product_paid_invoice/models/stock_picking.py
@@ -13,20 +13,21 @@ class Picking(models.Model):
     def validate_picking(self):
         """Set quantities automatically and validate the pickings."""
         for picking in self:
-            picking.action_assign()
-            for move in picking.move_lines.filtered(
-                    lambda m: m.state not in ["done", "cancel"]
-            ):
-                rounding = move.product_id.uom_id.rounding
-                if (
-                        float_compare(
-                            move.quantity_done,
-                            move.product_qty,
-                            precision_rounding=rounding,
-                        )
-                        == -1
+            if picking.sale_order_id:
+                picking.action_assign()
+                for move in picking.move_lines.filtered(
+                        lambda m: m.state not in ["done", "cancel"]
                 ):
-                    for move_line in move.move_line_ids:
-                        move_line.qty_done = move_line.product_uom_qty
-            picking.with_context(skip_immediate=True).button_validate()
-        return True
+                    rounding = move.product_id.uom_id.rounding
+                    if (
+                            float_compare(
+                                move.quantity_done,
+                                move.product_qty,
+                                precision_rounding=rounding,
+                            )
+                            == -1
+                    ):
+                        for move_line in move.move_line_ids:
+                            move_line.qty_done = move_line.product_uom_qty
+                picking.with_context(skip_immediate=True).button_validate()
+            return True
